c5cf684e73216698db42e09b9da0a8a3d2280b98
Sam <sam@Sams-MacBook.local>
2020-11-09 16:05:17 +0630

    change view

--- resources/views/bored.blade.php
+++ resources/views/bored.blade.php
@@ -7,24 +7,31 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Boring</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 
 <body class="bg-dark">
-    <div class="container mt-5">
-        <div class="row justify-content-center text-white">
-            <div class="col-md text-center">
-                <div class="card bg-warning" style="width: 25rem;">
-                    <div class="card-header h5">Bored To Do </div>
-                    <ul class="list-group  list-group-flush">
-                        <li class="list-group-item list-group-item-warning"><b>Activity: </b>{{ $data['activity'] }}</li>
-                        <li class="list-group-item list-group-item-warning"><b>Type:</b> {{ $data['type'] }}</li>
-                        <li class="list-group-item list-group-item-warning"><b>Participants:</b> {{ $data['participants'] }}</li>
-                        <li class="list-group-item list-group-item-warning"><b>Price: </b>{{ $data['price'] }}</li>
-                    </ul>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class=" mt-5">
+                <div class="card mb-3 bg-warning" style="width: 20rem;">
+                    <div class="card-header">
+                        Bored ToDo
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $data['activity'] }}</h5>
+                        <div class="card-text"><b>Type:</b> {{ $data['type'] }}</div>
+                        <div class="card-text"><b>Participants:</b> {{ $data['participants'] }}
+                        </div>
+                        <div class="card-text mb-3"><b>Price:</b> {{ $data['price'] }}</div>
+                        <a href="/" class="btn btn-secondary">New ToDo</a>
+                    </div>
                 </div>
             </div>
         </div>
+<!-- Footer -->
     </div>
+    
 </body>
 
 </html>
